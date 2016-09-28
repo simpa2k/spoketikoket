b8872346ef40bd5b065aa677a2f853bfe8ef2302
Simon Olofsson <s.olofsson@gmail.com>
2016-09-28 13:09:53 +0200

    Editing of videos and sounds now possible.

--- app/modules/coreModule/controllers/AdminEmbeddeditemsController.js
+++ app/modules/coreModule/controllers/AdminEmbeddeditemsController.js
@@ -5,20 +5,28 @@ define(function() {
 	app.controller('AdminEmbeddeditemsController', function($scope, $rootScope, $http, $filter, SendObjectService, EmbeddeditemsService) {
 
 		var embeddeditemsEndpoint = $rootScope.serverRoot + 'embeddeditems';
-	    $scope.embeddeditemsToBeSent = {};
+
+	    $scope.embeddeditemsToBeSent = {
+	        newEmbeddeditem: {}
+        };
 		$scope.embeddeditemToBeSent = {};
 
 	    var elementWithOpenEditingControls = null;
 
-		var toggleEditingControls = function(editingControls) {
+		var toggleEditingControls = function(element) {
+
+			let editingControls = element.find('.editing-controls');
+			let putStateButton= element.find('.put-state-button')
 
 			if(editingControls.hasClass('ng-hide')) {
 
 				editingControls.removeClass('ng-hide');
+				putStateButton.addClass('ng-hide');
 
 			} else {
 
 				editingControls.addClass('ng-hide');
+				putStateButton.removeClass('ng-hide');
 
 			}
 
@@ -26,38 +34,54 @@ define(function() {
 
 	    var showControls = function(elementId) {
 
-	        if(elementWithOpenEditingControls != null) {
-	        	elementWithOpenEditingControls.children('.editing-controls').addClass('ng-hide');
+            if(elementWithOpenEditingControls != null) {
+                toggleEditingControls(elementWithOpenEditingControls);
+            }
+
+            var element = $('#' + elementId);
+
+            if(element.is(elementWithOpenEditingControls)) {
+                elementWithOpenEditingControls = null;
+            } else {
+
+                elementWithOpenEditingControls = element;
+                toggleEditingControls(element);
+
+            }
+
+		};
+
+		$scope.hideControls = function() {
+
+			if(elementWithOpenEditingControls != null) {
+
+				toggleEditingControls(elementWithOpenEditingControls);
+                elementWithOpenEditingControls = null;
+
 			}
-
-	    	var element = $('#' + elementId);
-			var elementEditingControls = element.children('.editing-controls');
-
-			toggleEditingControls(elementEditingControls);
-
-			elementWithOpenEditingControls = element;
 
 		};
 
 	    $scope.setPutState = function(embeddeditem, index) {
 
-			$scope.embeddeditemsToBeSent[embeddeditem.type + '-' + index] = {
-				id: embeddeditem.id,
-				src: embeddeditem.src,
-				type: embeddeditem.type
-			};
+            let uniqueId = embeddeditem.type + '-' + index;
+            let objectCopy = $scope.embeddeditemsToBeSent[uniqueId];
 
-	        $scope.embeddeditemToBeSent.id = embeddeditem.id;
-	        $scope.embeddeditemToBeSent.src = embeddeditem.src;
-			$scope.embeddeditemToBeSent.type = embeddeditem.type;
+            if(typeof(objectCopy) == 'undefined') {
+                objectCopy = jQuery.extend({}, embeddeditem);
+                delete objectCopy.$$hashKey;
+
+                $scope.embeddeditemsToBeSent[uniqueId] = objectCopy;
+            }
+
+			$scope.embeddeditemToBeSent = objectCopy;
 
 			console.log($scope.embeddeditemsToBeSent);
 			console.log($scope.embeddeditemToBeSent);
 
-			showControls(embeddeditem.type + '-' + index);
+			showControls(uniqueId);
 
 			$scope.embeddeditemAction = 'Bekräfta ändringar';
-            $scope.editButtonText = 'Avsluta redigeringsläge';
 			$scope.heading = 'Redigera klipp';
             $scope.addingNewEmbeddeditem = false;
 
@@ -68,7 +92,6 @@ define(function() {
 	        $scope.embeddeditemToBeSent = {};
 
 			$scope.embeddeditemAction = 'Lägg till klipp';
-			$scope.editButtonText = 'Redigera';
 			$scope.heading = 'Lägg till nytt klipp';
 			$scope.addingNewEmbeddeditem = true;
 
@@ -80,12 +103,23 @@ define(function() {
 	        EmbeddeditemsService.refreshEmbeddeditems().then(function(embeddeditems){
 	            $scope.embeddeditems = embeddeditems;
 	        });
+
+            EmbeddeditemsService.refreshVideos().then(function(videos) {
+                $scope.videos = videos;
+            });
+
+            EmbeddeditemsService.refreshSounds().then(function(sounds) {
+                $scope.sounds= sounds;
+            });
 	    };
 	
 	    $scope.postEmbeddeditem = function() {
+
+	        $scope.embeddeditemToBeSent = $scope.embeddeditemsToBeSent['newEmbeddeditem'];
+
 	        SendObjectService.postObject(embeddeditemsEndpoint, $scope.embeddeditemToBeSent, function() {
 	            refreshEmbeddeditems();
-	            $scope.setPostState();
+	            //$scope.setPostState();
 	        });
 	    };
 	
@@ -98,7 +132,7 @@ define(function() {
 	    $scope.deleteEmbeddeditem = function() {
 	        SendObjectService.deleteObject(embeddeditemsEndpoint, $scope.embeddeditemToBeSent, function() {
 	            refreshEmbeddeditems();
-	            $scope.setPostState();
+	            //$scope.setPostState();
 	        });
 	    };
 
