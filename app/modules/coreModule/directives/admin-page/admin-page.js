327c2d96e12b5f44e4626c020c17d2c0e8acdaa1
Simon Olofsson <s.olofsson@gmail.com>
2017-04-12 21:45:36 +0200

    Added documentation.

--- app/modules/coreModule/directives/admin-page/admin-page.js
+++ app/modules/coreModule/directives/admin-page/admin-page.js
@@ -1,45 +1,109 @@
 define(function() {
 
+    /**
+     * Directive for defining an admin page. Expects a set of data items to
+     * display and make available for modification and an array of objects
+     * defining the structure of the form needed to modify and create such
+     * items. Each object in the array should describe one form-group by
+     * specifying a label for the form-group and providing an object of field
+     * names mapped to a specific input type. For example:
+     *
+     * let $scope.formStructure = [
+     *  {
+     *      label: 'Meta data:',
+     *      fields: {
+     *          datetime: 'datetime',
+     *          author: 'text'
+     *      }
+     *  },
+     *  {
+     *      label: 'Content:',
+     *      fields: {
+     *          content: 'textfield'
+     *      }
+     *  }
+     *]
+     */
     let adminPage =  function() {
 
         let controller = ['$scope', function($scope) {
 
-            $scope.objectToSend = {};
-
+            /**
+             * Function to populate form fields with information from one data
+             * item, for modification.
+             *
+             * @param item The item to modify.
+             */
             $scope.setPutState = function(item) {
 
+                // Copying selected item to ensure nothing is changed before the confirm button is clicked.
                 $scope.objectToSend = angular.copy(item);
 
-                $scope.addingNew = false;
+                $scope.addingNew = false; // This displays the confirm and delete buttons.
                 $scope.action = "Bekräfta ändringar";
 
+                /*
+                 * $scope.send is the function actually called on clicking the
+                 * button on the page. Since we're preparing for a PUT, the
+                 * $scope.send is set to refer to the appropriate function.
+                 */
                 $scope.send = $scope.put;
 
             };
 
+            /**
+             * Function to empty form fields and prepare for creating a new
+             * data item.
+             */
             $scope.setPostState = function() {
 
-                $scope.objectToSend = {};
+                $scope.objectToSend = {}; // Emptying the object to be POSTed.
 
                 $scope.action = "Lägg till";
-                $scope.addingNew = true;
+                $scope.addingNew = true; // This hides the confirm and delete buttons.
 
+                /*
+                 * $scope.send is the function actually called on clicking the
+                 * button on the page. Since we're preparing for a POST, the
+                 * $scope.send is set to refer to the appropriate function.
+                 */
                 $scope.send = $scope.post;
 
             };
 
+            /**
+             * Function to perform a PUT on a data item.
+             *
+             * @param formName The name of the form to reset upon completion of
+             * the request.
+             */
             $scope.put = function(formName) {
                 console.log($scope.objectToSend);
             };
 
+            /**
+             * Function to POST a data item.
+             *
+             * @param formName The name of the form to reset upon completion of
+             * the request.
+             */
             $scope.post = function(formName) {
                 console.log($scope.objectToSend);
             };
 
+            /**
+             * Function to DELETE a data item.
+             *
+             * @param formName The name of the form to reset upon completion of
+             * the request.
+             */
             $scope.delete = function(formName) {
                 console.log($scope.objectToSend);
             };
 
+            /*
+             * Initializing admin page with an empty form.
+             */
             $scope.setPostState();
 
         }];
--- app/modules/coreModule/directives/admin-page/dynamic-input.js
+++ app/modules/coreModule/directives/admin-page/dynamic-input.js
@@ -1,19 +1,40 @@
 define(function() {
 
+    /**
+     * Directive to generate an appropriate input based on a specified type.
+     * Currently, the following types are supported:
+     *
+     *  * datetime
+     *  * text
+     *
+     * The datetime type generates one uib-datepicker-popup and one
+     * uib-timepicker from the UI Bootstrap library.
+     */
     let dynamicInput = function($compile) {
 
         let controller = ['$scope', function($scope) {
 
+            // Used by date inputs to control whether the popup should be displayed or not.
             $scope.datePopup = {
                 opened: false
             };
 
+            // Called on clicking the datepicker button.
             $scope.openDatePopup = function() {
                 $scope.datePopup.opened = true;
             };
 
         }];
 
+        /**
+         * Function to get an object with standard attributes set based on a
+         * given field name, to be used for programmatically setting the
+         * attributes of an angular element.
+         *
+         * @param fieldName The name of the field that the input maps to.
+         * @returns {{id: *, class: string, ngModel: string, placeholder: *}}
+         * An object with a set of standard keys and values.
+         */
         let getStandardAttributes = function(fieldName) {
 
             return {
@@ -26,11 +47,23 @@ define(function() {
             }
         };
 
+        /**
+         * Function to programmatically set the attributes of an angular
+         * element based on an object containing the relevant key-value
+         * mappings. Will convert from camelCased key names to snake-cased
+         * ones.
+         *
+         * @param element The element to set the attributes of.
+         * @param attributes An object to extract key-value attributes from.
+         * @param scope The current scope, needed to be able to compile the
+         * element upon successful setting of the attributes.
+         */
         let buildAttributes = function(element, attributes, scope) {
 
             angular.forEach(attributes, function(value, key) {
 
                 /*
+                 * Converting from camelCase to snake-case.
                  * From a comment on the second answer at:
                  * http://stackoverflow.com/questions/24357288/how-to-convert-dash-case-to-camelcase-in-angularjs
                  */
@@ -41,11 +74,19 @@ define(function() {
                 element.attr(snakeCasedKey, value);
 
             });
-            $compile(element)(scope);
+            $compile(element)(scope); // This is needed to apply the performed changes.
 
         };
 
-        let appendInput = function(parent, fieldName, attributes, scope) {
+        /**
+         * Function to create and append a standard input element to a given parent.
+         *
+         * @param parent The element to append the newly created input element
+         * to.
+         * @param fieldName The name of the field that the input maps to.
+         * @param scope The current scope.
+         */
+        let appendInput = function(parent, fieldName, scope) {
 
             let standardAttributes = getStandardAttributes(fieldName);
             standardAttributes.type = 'text';
@@ -54,8 +95,19 @@ define(function() {
 
         };
 
+        /**
+         * Function to create and append a date input element to a given parent.
+         * Will create a UI Bootstrap datepicker which means that if this
+         * field is initialized with a model already containing a value, that
+         * value has to be a javascript date object.
+         *
+         * @param parent The element to append the newly created date input to.
+         * @param fieldName The name of the field that the input maps to.
+         * @param scope The current scope.
+         */
         let appendDateInput = function(parent, fieldName, scope) {
 
+            // Creating a <p> to wrap the date input and the button in.
             let p = $compile('<p class="input-group"></p>')(scope);
 
             let standardAttributes = {
@@ -67,38 +119,61 @@ define(function() {
 
             };
 
-            standardAttributes.type = 'text';
-            standardAttributes.uibDatepickerPopup = 'd/M, yyyy';
-            standardAttributes.isOpen = 'datePopup.opened';
+            standardAttributes.type = 'text'; // Type should be set to 'text' to avoid browsers displaying their own datepickers.
+            standardAttributes.uibDatepickerPopup = 'd/M, yyyy'; // "April 12th 2017" will be displayed as "12/4, 2017".
+            standardAttributes.isOpen = 'datePopup.opened'; // Specifying the variable to store open/closed state.
 
 
+            // Doing a bit of translating from the standard button texts.
             standardAttributes.currentText = 'Dagens datum';
             standardAttributes.clearText = 'Rensa';
             standardAttributes.closeText = 'Stäng';
 
+            // Appending an input with the above attributes to the <p> created above. Creates a UI Bootstrap datepicker popup.
             appendElement(p, 'input', standardAttributes, scope);
+
+            // Creating the  button that will actually display the popup on clicking it.
             let btn = $compile('<span class="input-group-btn"><button type="button" class="btn btn-default" ng-click="openDatePopup()"><i class="glyphicon glyphicon-calendar"></i></button></span>')(scope);
 
-            p.append(btn);
-            $compile(p)(scope);
+            p.append(btn); // Append the button to the <p>.
+            $compile(p)(scope); // Without this, the datepicker popup would not be displayed on clicking the button.
 
-            parent.append(p);
+            parent.append(p); // Attach everything to the wrapping element.
 
         };
 
+        /**
+         * Function to create and append a time input element to a given parent.
+         * Will create a UI Bootstrap timepicker which means that if this
+         * field is initialized with a model already containing a value, that
+         * value has to be a javascript date object.
+         *
+         * @param parent The element to append the newly created date input to.
+         * @param fieldName The name of the field that the input maps to.
+         * @param scope The current scope.
+         */
         let appendTimeInput = function(parent, fieldName, scope) {
 
             let standardAttributes = getStandardAttributes(fieldName);
 
-            delete standardAttributes.class;
+            delete standardAttributes.class; // The input should not have the form-group class.
 
-            standardAttributes.uibTimepicker = "";
-            standardAttributes.showMeridian = false;
+            standardAttributes.uibTimepicker = ""; // No value needed.
+            standardAttributes.showMeridian = false; // The default is null, but setting it for good measure.
 
-            appendElement(parent, 'div', standardAttributes, scope);
+            appendElement(parent, 'div', standardAttributes, scope); // Create the timepicker input.
 
         };
 
+        /**
+         * Function to create and append an angular element to a given parent.
+         *
+         * @param parent The parent to append the newly created element to.
+         * @param element The element to create in the form of a string.
+         * @param attributes An object containing key-value mappings to extract
+         * attributes and values from.
+         * @param scope The current scope.
+         */
         let appendElement = function(parent, element, attributes, scope) {
 
             let input = $compile('<' + element + '></' + element + '>')(scope);
@@ -126,7 +201,7 @@ define(function() {
                         appendTimeInput(element, attributes.fieldName, scope);
                         break;
                     default:
-                        appendInput(element, attributes.fieldName, attributes, scope);
+                        appendInput(element, attributes.fieldName, scope);
                         break;
 
                 }
